ae00c9577b75deb7a6adf1658a04745d6b147876
Daniel Klintworth <daniel.klintworth@gmail.com>
2015-04-04 23:09:36 +0200

    complete string manipulation and tuples

--- python2/koans/about_string_manipulation.py
+++ python2/koans/about_string_manipulation.py
@@ -35,13 +35,13 @@ class AboutStringManipulation(Koan):
         self.assertEqual("a", string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        self.assertEqual(97, ord('a'))
+        self.assertEqual(True, ord('b') == (ord('a') + 1))
 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertEqual([__, __, __], words)
+        self.assertEqual(["Sausage", "Egg", "Cheese"], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
         import re  # import python regular expression library
@@ -51,7 +51,7 @@ class AboutStringManipulation(Koan):
 
         words = pattern.split(string)
 
-        self.assertEqual([__, __, __, __], words)
+        self.assertEqual(["the", "rain", "in", "spain"], words)
 
         # `pattern` is a Python regular expression pattern which matches
         # ',' or ';'
@@ -59,18 +59,18 @@ class AboutStringManipulation(Koan):
     def test_raw_strings_do_not_interpret_escape_characters(self):
         string = r'\n'
         self.assertNotEqual('\n', string)
-        self.assertEqual(__, string)
-        self.assertEqual(__, len(string))
+        self.assertEqual(r"\n", string)
+        self.assertEqual(2, len(string))
 
         # Useful in regular expressions, file paths, URLs, etc.
 
     def test_strings_can_be_joined(self):
         words = ["Now", "is", "the", "time"]
-        self.assertEqual(__, ' '.join(words))
+        self.assertEqual("Now is the time", ' '.join(words))
 
     def test_strings_can_change_case(self):
-        self.assertEqual(__, 'guido'.capitalize())
-        self.assertEqual(__, 'guido'.upper())
-        self.assertEqual(__, 'TimBot'.lower())
-        self.assertEqual(__, 'guido van rossum'.title())
-        self.assertEqual(__, 'ToTaLlY aWeSoMe'.swapcase())
+        self.assertEqual("Guido", 'guido'.capitalize())
+        self.assertEqual("GUIDO", 'guido'.upper())
+        self.assertEqual("timbot", 'TimBot'.lower())
+        self.assertEqual("Guido Van Rossum", 'guido van rossum'.title())
+        self.assertEqual("tOtAlLy AwEsOmE", 'ToTaLlY aWeSoMe'.swapcase())
