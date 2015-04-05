9b49a486b02f83b5caeeb2a5dc9753dfbff699c6
Daniel Klintworth <daniel.klintworth@gmail.com>
2015-04-05 23:37:56 +0200

    triangles. Interesteing example of using sets

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,7 +19,14 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    sides = {a, b, c}
+    if len(sides) == 1: # if all 3 are equal, 2 should be ignored in the set, leaving just 1
+	return 'equilateral'
+    elif len(sides) == 2: # if two are equal, there must be 2 distinct values left
+	return 'isosceles'
+    elif len(sides) == 3: # if no sides are equal, we will get three distinct values
+	return 'scalene'
+    
 
 
 # Error class used in part 2.  No need to change this code.
